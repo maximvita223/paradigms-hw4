9bb90d3adb0a3248b3bad33312cb8a253a289508
Maxim <Max@example.com>
2023-08-28 15:17:56 +0300

    Сделал для 1 задания факториал

--- task01.py
+++ task01.py
@@ -6,3 +6,12 @@ def rec_sum(n):
         return result + rec_sum(n-1)
 
 print(rec_sum(4))
+
+def factorial(n):
+    result = int(n)
+    if n <= 1:
+        return result 
+    else:
+        return result * factorial(n-1)
+
+print(factorial(5))
